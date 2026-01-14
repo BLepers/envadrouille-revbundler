669773d849f711ff8c9e76a1e54e68250dcbe300
Baptiste Lepers <baptiste.lepers@gmail.com>
2026-01-14 17:03:44 +0100

    Port the updater script to git now that the mercurial envadrouille repo is gone

--- create_new_version.pl
+++ create_new_version.pl
@@ -11,108 +11,105 @@ use File::Basename;
 #The new version of the files are stored with filenames xxx.ext.last
 #The old version of the files are stored with filenames xxx.ext.orig
 
-my $path_to_hg = '../envadrouille';
+my $path_to_git = '../envadrouille';
 my $current_path = getcwd;
-chdir($path_to_hg) or die("Directory $path_to_hg does not exist\n");
+chdir($path_to_git) or die("Directory $path_to_git does not exist\n");
+
+
 
 #Create a new stable branch
+print `git checkout master`;
 my ($sec, $min, $h, $day, $month, $year, @tmp) = localtime();
 my $newrev = sprintf("%02d%02d%02d", $year % 100, $month+1, $day);
-open(F, "$path_to_hg/admin/index.php");
+open(F, "$path_to_git/admin/index.php");
 my @indlines = <F>;
 close(F);
-open(F, "> $path_to_hg/admin/index.php");
+open(F, "> $path_to_git/admin/index.php");
 for my $l (@indlines) {
-   if($l =~ /^\$VERSION/) {
-      $l = "\$VERSION = '$newrev';\n";
-   }
-   print F $l;
+	if($l =~ /^\$VERSION/) {
+		$l = "\$VERSION = '$newrev';\n";
+	}
+	print F $l;
 }
 close(F);
-print "(cd $path_to_hg && hg update default && hg tag stable-$newrev)\n";
-print `(cd $path_to_hg && hg update default && hg commit -m 'New revision' && hg tag stable-$newrev)\n`;
+my $diff = `git diff`;
+if($diff ne '') {
+	print "(git commit -a -m 'New revision' && git tag stable-$newrev)\n";
+	print `(git commit -a -m 'New revision' && git tag stable-$newrev)\n`;
+} else {
+	print "No changes to commit bypassing commit and tag creation\n";
+}
 
 #Get list of stable revs
-my %tagsrev;
 my @tags = reverse sort map {
-      if($_ =~ m/(stable-\d+)\s*(\d+):(\w+)/) {
-         my $rev = $1;
-         $tagsrev{$rev} = $2;
-         $rev =~ m/(\d+)/;
-         if($1 < 130219) {
-            ();
-         } else {
-            $rev;
-         }
-      } else {
-         ();
-      }
-   } split(/\n/, `hg tags`);
+	if($_ =~ m/(stable-\d+)/) {
+		$1;
+	} else {
+		();
+	}
+} split(/\n/, `git tag`);
 print Dumper(\@tags);
 
 #Create diffs for each rev
-#A diff =
-for my $revn (1..$#tags) {
-   chdir($current_path);
+for my $tag (@tags) {
+	next if($tag eq $tags[0]); #Skip the latest tag
 
-   print "(cd $path_to_hg && hg update $tags[$revn])\n";
-   print `(cd $path_to_hg && hg update $tags[$revn])`;
-
-   #Get list of changed files. Ignores hidden files (.xxx)
-   my @diff = map {
-    /^diff -r \w+ ([^\.].*)$/ ? { "file" => $1 } : ()
-   } split(/\n/, `(cd $path_to_hg && hg diff -r $tagsrev{$tags[0]})`);
+	#Get list of changed files. Ignores hidden files (.xxx)
+	print "git diff --name-only $tag..master\n";
+	my @diff = `git diff --name-only $tag..master`;
+	chomp @diff;
 
 
-   my $patch_dir = "./envadrouille";
-   for my $ff (@diff) {
-      my $f = $ff->{file};
+	my @json;
+	my $patch_dir = "$current_path/envadrouille";
+	for my $f (@diff) {
+		my $cmd = "mkdir -p $patch_dir/".dirname($f);
+		print `$cmd`;
+		print `git show $tag:$f > $patch_dir/$f.orig`;
+		print `git show master:$f > $patch_dir/$f.last`;
+		push(@json, { file => $f, binary => is_binary("$patch_dir/$f.last") });
+	}
 
-      my $cmd = "mkdir -p $patch_dir/".dirname($f);
-      print `$cmd`;
-      print `cat $path_to_hg/$f > $patch_dir/$f.orig`;
-      #We must 'cd' to the path otherwise if the current path is also an hg rep, hg gets confused
-      print `(cd $path_to_hg && hg cat -r $tagsrev{$tags[0]} $f) > $patch_dir/$f.last`; 
-      $ff->{binary} = is_binary("$patch_dir/$f.last");
-   }
+	open(F, "> $current_path/diff.json");
+	print F to_json(\@json);
+	close(F);
 
-   open(F, "> diff.json");
-   print F to_json(\@diff);
-   close(F);
+	chdir($current_path);
+	print `zip -r patch-$tag-$tags[0].zip diff.json ./envadrouille`;
+	print `rm -rf diff.json ./envadrouille`;
+	chdir($path_to_git);
 
-   print `zip -r patch-$tags[$revn]-$tags[0].zip diff.json $patch_dir`;
-   print `rm -rf diff.json $patch_dir`;
-
-   print "(cd $path_to_hg && hg update && hg log --style changelog -P $tags[$revn] -b default -X .hgtags) | tail -n +2 > CHANGELOG-$tags[$revn]-$tags[0]\n";
-   print `(cd $path_to_hg && hg update && hg log --style changelog -P $tags[$revn] -b default -X .hgtags) | tail -n +2 > CHANGELOG-$tags[$revn]-$tags[0]`;
-   my $changes = `cat CHANGELOG-$tags[$revn]-$tags[0]`;
-   open(F, "> CHANGELOG-$tags[$revn]-$tags[0]");
-   print F 'showChangelog('.to_json({'changelog' => $changes}).')';
-   close(F);
+	print "git log $tag..master --pretty=format:'* %s' > $current_path/CHANGELOG-$tag-$tags[0]\n";
+	print `git log $tag..master --pretty=format:'* %s' > $current_path/CHANGELOG-$tag-$tags[0]\n`;
+	my $changes = `cat $current_path/CHANGELOG-$tag-$tags[0]`;
+	open(F, "> $current_path/CHANGELOG-$tag-$tags[0]");
+	print F 'showChangelog('.to_json({'changelog' => $changes}).')';
+	close(F);
 }
 
-print "(cd $path_to_hg && hg update && hg archive $current_path/envadrouille.zip)\n";
-print `(cd $path_to_hg && hg update && hg archive $current_path/envadrouille.zip)`;
+print `git checkout master`;
+print "git archive --format=zip -o $current_path/envadrouille.zip master\n";
+print `(git archive --format=zip -o $current_path/envadrouille.zip master)\n`;
 
 print "(cp $current_path/envadrouille.zip $current_path/latest.zip)\n";
 print `(cp $current_path/envadrouille.zip $current_path/latest.zip)`;
 
+chdir($current_path);
 open(F, "> VERSION");
 my ($revdate) = ($tags[0] =~ m/stable-(\d+)/);
 print F "remote_check_version({\"version\":$revdate})";
 close(F);
 
 open(F, "> VERSION-JSON");
-my ($revdate) = ($tags[0] =~ m/stable-(\d+)/);
 print F "[\"$revdate\"]";
 close(F);
 
 sub is_binary {
-   my ($f) = @_;
-   my $meta = `file $f`;
-   if($meta =~ /text/) {
-      return 0;
-   } else {
-      return 1;
-   }
+	my ($f) = @_;
+	my $meta = `file $f`;
+	if($meta =~ /text/) {
+		return 0;
+	} else {
+		return 1;
+	}
 }
